d04e0f71eec4b32d75b99892abe9e6a261739e1a
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-11-22 15:42:23 -0600

    file now saves to uploads folder. Renamed dir  audio to uploads

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import *  
+from flask import Flask, request, render_template
+import pprint
 # from transcribe import scrib 
 app = Flask(__name__)  
- 
+
 @app.route('/')  
 def upload():  
     return render_template("upload.html")  
  
-@app.route('/success', methods = ['POST'])  
+@app.route('/success', methods = ['GET','POST'])  
 def success():  
     if request.method == 'POST':  
         f = request.files['file']  
-        f.save(f.filename)  
+        f.save('uploads/' + f.filename)  
 
     # execute transcribe function with argument of f
 
--- upload.py
+++ upload.py
@@ -6,11 +6,11 @@ from transcribe import *
 import config as cf 
 
 bucketname = cf.bucketname
-audio_file_name = 'audio/test1.mp3'
+audio_file_name = 'audio/bled.mp3'
 
-upload_blob( bucketname, audio_file_name , 'test5.mp3')
+upload_blob( bucketname, audio_file_name , 'bled.mp3')
 
-gcs_uri = 'gs://' + bucketname + '/' + 'test5.mp3'
+gcs_uri = 'gs://' + bucketname + '/' + 'bled.mp3'
 
 sample_long_running_recognize(gcs_uri)
 ## TO DO: delete the blob after upload.
